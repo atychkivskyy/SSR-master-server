f645af9f019070a0747dd5f9039c73431603c051
atychkivskyy <124183654+atychkivskyy@users.noreply.github.com>
2024-12-16 17:42:23 +0100

    Update app.js

--- app.js
+++ app.js
@@ -44,8 +44,8 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
-app.listen(3000, () => {
-  console.log(`Server is running`);
+app.listen(3000, '0.0.0.0', () => {
+    console.log('Server is running on port 3000');
 });
 
 module.exports = app;
